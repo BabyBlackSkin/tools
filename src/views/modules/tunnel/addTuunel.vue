7ce06761b091a7f96b09a73da0ab7f706d95c393
Qinyubin <qin.yubin@mobcb.com>
2024-03-22 15:06:01 +0800

    修复SSH修改功能， 支持在修改和新增的时候测试隧道

--- src/views/modules/tunnel/addTuunel.vue
+++ src/views/modules/tunnel/addTuunel.vue
@@ -48,6 +48,7 @@
         <el-form-item>
           <el-button @click="cancel">取 消</el-button>
           <el-button type="primary" @click="submit">确 定</el-button>
+          <el-button type="success" @click="test">测 试</el-button>
         </el-form-item>
       </el-form>
     </el-row>
@@ -136,13 +137,23 @@ export default {
         this.form = res.data[0]
       })
     },
-    submit() {
-      this.$refs.form.validate((valid) => {
-        if (!valid) {
-          return
-        }
+    getSql() {
+      if (this.form.id) {
+        return `UPDATE "main"."tunnel_ssh_config"
+         SET "name" = '${this.form.name}',
+        "src_port" = '${this.form.src_port}',
+        "dst_host" = '${this.form.dst_host}',
+        "dst_port" = '${this.form.dst_port}',
+        "ssh_host" = '${this.form.ssh_host}',
+        "ssh_port" = '${this.form.ssh_port}',
+        "ssh_user_name" = '${this.form.ssh_user_name}',
+        "ssh_password" = '${this.form.ssh_password}'
+        WHERE
+        "id" = ${this.form.id};
+        `
+      } else {
 
-        let sql = `
+        return `
           INSERT INTO "main"."tunnel_ssh_config"
           ("name", "src_port", "dst_host", "dst_port", "ssh_host", "ssh_port", "ssh_user_name", "ssh_password")
           VALUES
@@ -156,6 +167,15 @@ export default {
             "${this.form.ssh_password}"
             )
         `
+      }
+    },
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (!valid) {
+          return
+        }
+        let sql = this.getSql()
+
         window.sqlite.execute(sql).then(res => {
           this.$notify({
             title: res.result ? '成功' : '警告',
@@ -165,6 +185,39 @@ export default {
           });
           this.$router.push({name: 'sshList'})
         })
+      })
+    },
+    test() {
+      let config = {
+        sshOptions: {
+          host:  this.form.ssh_host,
+          port: this.form.ssh_port,
+          username: this.form.ssh_user_name,
+          password: this.form.ssh_password,
+        },
+        forwardOptions: {
+          dstAddr: this.form.dst_host,
+          dstPort: this.form.dst_port,
+          srcPort: this.form.src_port
+        }
+      }
+      window.ssh.connect(config).then(res => {
+        if (!res.result) {
+          this.$message({
+            title: '失败',
+            message: res.msg,
+            type: 'error',
+            duration: 2000
+          });
+          return
+        }
+        this.$message({
+          title: '成功',
+          message: res.msg,
+          type: 'success',
+          duration: 2000
+        });
+        window.ssh.close();
       })
     },
     cancel() {
